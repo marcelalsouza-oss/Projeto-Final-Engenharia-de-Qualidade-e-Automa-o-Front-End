4a31c113e11931a2a8b696fe4218a6a7e23798d2
lulu★ <maria.l.oliveira7@ba.estudante.senai.br>
2026-04-22 08:34:42 -0300

    Add favorite functionality for dog images

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 const API_KEY = "live_7gaBlmKKZul8IM4Nv2kM0qvp2U6wMrbtqrZS5gDagmFEZYD2zTejSDatOEYPBuBO";
-
 const cards = document.querySelectorAll(".dog-card");
+
+function getFavorites() {
+  return JSON.parse(localStorage.getItem('pinthedogs_favorites') || '[]');
+}
+
+function saveFavorites(favorites) {
+  localStorage.setItem('pinthedogs_favorites', JSON.stringify(favorites));
+}
+
+function toggleFavorite(card, img, checkbox) {
+  let favorites = getFavorites();
+
+  const existingIndex = favorites.findIndex(f => f.url === img.src);
+
+  if (checkbox.checked) {
+    if (existingIndex === -1) {
+      favorites.push({
+        name: card.dataset.dogName,
+        url: img.src
+      });
+    }
+  } else {
+    if (existingIndex !== -1) {
+      favorites.splice(existingIndex, 1);
+    }
+  }
+
+  saveFavorites(favorites);
+}
 
 async function carregarDogs() {
   try {
@@ -14,12 +42,23 @@ async function carregarDogs() {
     );
 
     const data = await response.json();
+    const favorites = getFavorites();
 
     cards.forEach((card, index) => {
       const img = card.querySelector("img");
+      const checkbox = card.querySelector('.heart-check');
 
       if (data[index]) {
         img.src = data[index].url;
+
+        // Marca se já é favorito
+        const isFav = favorites.some(fav => fav.url === img.src);
+        checkbox.checked = isFav;
+
+        // Evento de curtir
+        checkbox.addEventListener('change', () => {
+          toggleFavorite(card, img, checkbox);
+        });
       }
     });
 
