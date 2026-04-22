f3028bd347bd249db180e997d41c9cbf834a7ca0
lulu★ <maria.l.oliveira7@ba.estudante.senai.br>
2026-04-22 09:38:15 -0300

    Update test to use local file path

--- teste1.cy.js
+++ teste1.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:5500/index.html')
+    cy.visit('file:///C:/Users/aluno.senai/Desktop/pindogs/index.html')
     cy.get('body').should('be.visible')
     cy.get('#heart1').click({force:true})
     cy.get('input[type="text"][placeholder="Pesquise seu aumigo..."]').type('Dog 1')
