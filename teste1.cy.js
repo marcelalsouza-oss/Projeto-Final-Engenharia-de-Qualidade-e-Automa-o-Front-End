c5ff70db642436ce81077342c99c2235ae5b7826
lulu★ <maria.l.oliveira7@ba.estudante.senai.br>
2026-04-22 13:40:10 -0300

    Refactor Cypress tests for dog image application

--- teste1.cy.js
+++ teste1.cy.js
@@ -1,8 +1,77 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('file:///C:/Users/aluno.senai/Desktop/pindogs/index.html')
-    cy.get('body').should('be.visible')
-    cy.get('#heart1').click({force:true})
-    cy.get('input[type="text"][placeholder="Pesquise seu aumigo..."]').type('Dog 1')
-  })
-})
+
+  beforeEach(() => {
+    cy.intercept('GET', '**/api.thedogapi.com/v1/images/search*').as('getDogs');
+
+    cy.visit("/index.html");
+
+    cy.window().then((win) => {
+      win.localStorage.clear();
+    });
+  });
+
+  it('page loads correctly', () => {
+    cy.get('body').should('be.visible');
+  });
+
+  it("Página carrega corretamente", () => {
+    cy.get(".header").should("be.visible");
+    cy.get(".dog-grid").should("be.visible");
+  });
+
+  it("Confirma se as imagens dos cães estão sendo exibidas corretamente", () => {
+    cy.wait('@getDogs', { timeout: 10000 });
+
+    cy.get(".dog-card img", { timeout: 10000 })
+      .should("have.length.at.least", 1);
+
+    cy.get(".dog-card img").each(($img) => {
+      cy.wrap($img)
+        .should("be.visible")
+        .and(($el) => {
+          expect($el[0].src).to.not.be.empty;
+        });
+    });
+
+    cy.get(".dog-card").should("have.length.at.least", 1);
+  });
+
+  it("Imagens da API aparecem", () => {
+    cy.wait('@getDogs', { timeout: 10000 });
+
+    cy.get(".dog-card img", { timeout: 10000 })
+      .should("have.length.at.least", 1);
+  });
+
+  it("Navega para favoritos.html", () => {
+    cy.get(".favorite-btn").click();
+    cy.url().should("include", "favoritos.html");
+    cy.contains("Favoritos").should("be.visible");
+  });
+
+  it("Mostra mensagem quando não há favoritos", () => {
+    cy.visit("/favoritos.html");
+
+    cy.contains("Nenhum favorito ainda!")
+      .should("be.visible");
+  });
+
+  it("Renderiza favoritos salvos no localStorage", () => {
+    cy.window().then((win) => {
+      win.localStorage.setItem(
+        "pinthedogs_favorites",
+        JSON.stringify([
+          { url: "https://example.com/dog1.jpg", name: "Dog 1" },
+          { url: "https://example.com/dog2.jpg", name: "Dog 2" }
+        ])
+      );
+    });
+
+    cy.visit("/favoritos.html");
+
+    cy.get(".dog-card img", { timeout: 10000 })
+      .should("have.length", 2)
+      .and("be.visible");
+  });
+
+});
